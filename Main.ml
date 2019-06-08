bf9c72463c6cdf7f00e04bf095dca1007dad48f0
Gustav Behm <me@rootmos.io>
2019-06-08 11:28:54 +0200

    Add a small continuation style prototype

--- Main.ml
+++ Main.ml
@@ -1,1 +1,12 @@
-let () = print_endline "<html><body>hello</body></html>"
+open Printf
+
+let tag t k x = sprintf "<%s>%s</%s>" t (k x) t
+
+let html = tag "html"
+let body = tag "body"
+
+let text t () = t
+
+let index = html @@ body @@ text "lol"
+
+let () = print_endline (index ())
