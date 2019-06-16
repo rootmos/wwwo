6ef83b47bc4341b17c0c343984517f8e022a25b0
root <root@www.rootmos.io>
2019-06-16 15:42:24 +0000

    Add the rebuild script

---
+++ rebuild.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(readlink -f $0 | xargs dirname)
+
+MAKE=$(which gmake 2>/dev/null)
+if [ $? -ne 0 ]; then
+    MAKE=$(which make)
+fi
+
+(cd $SCRIPT_DIR && git pull 2>&1)
+$MAKE -C $SCRIPT_DIR refresh generate
