db1d9d335d075e7c3f681bb2546c931953bed022
Gustav Behm <me@rootmos.io>
2019-08-06 20:20:31 +0200

    Only process root-level sounds

--- sounds.py
+++ sounds.py
@@ -10,6 +10,6 @@ s3 = session.resource("s3", endpoint_url="https://ams3.digitaloceanspaces.com")
 pool = ThreadPoolExecutor(8)
 
 bucket = s3.Bucket("rootmos-sounds")
-os = filter(lambda o: o.key.endswith(".json"), bucket.objects.all())
+os = filter(lambda o: o.key.endswith(".json") and "/" not in o.key, bucket.objects.all())
 ss = pool.map(lambda o: json.loads(o.get()["Body"].read()), os)
 print(json.dumps(list(ss), separators=(',', ':')))
