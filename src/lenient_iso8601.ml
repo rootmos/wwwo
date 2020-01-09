6d3486d913635057763f7fe7af1d110f2ee330cd
Gustav Behm <me@rootmos.io>
2020-01-09 08:44:45 +0100

    Correct the sound sorting to include time

--- src/lenient_iso8601.ml
+++ src/lenient_iso8601.ml
@@ -1,13 +1,17 @@
-type t = CalendarLib.Date.t
+open CalendarLib
+type t = Calendar.t
 
 let wrap s =
-  try CalendarLib.Printer.Date.from_fstring "%F" s
+  try Printer.Calendar.from_fstring "%F" s
   with Invalid_argument _ ->
-  try CalendarLib.Printer.Date.from_fstring "%FT%TZ" s
+  try Printer.Calendar.from_fstring "%FT%TZ" s
   with Invalid_argument _ ->
-  CalendarLib.Printer.Date.from_fstring "%FT%T%:z" s
+  try Printer.Calendar.from_fstring "%FT%T%:z" s
+  with Invalid_argument _ ->
+  let d = Printer.Date.from_fstring "%F" s in
+  Calendar.create d (Time.midday ())
 
-let unwrap t = CalendarLib.Printer.Date.sprint "%a, %d %b %Y" t
+let unwrap t = Printer.Calendar.sprint "%a, %d %b %Y" t
 
-let rfc822 t = CalendarLib.Printer.Date.sprint "%a, %d %b %Y" t
-let compare t t' = CalendarLib.Date.compare t t'
+let rfc822 t = Printer.Calendar.sprint "%a, %d %b %Y" t
+let compare t t' = Calendar.compare t t'
