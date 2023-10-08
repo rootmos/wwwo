15e69777630df5a71f3e9972d26841b630936b0d
Gustav Behm <me@rootmos.io>
2023-10-08 09:56:09 +0200

    Publish errors to a SNS topic

--- lambda/src/Lambda/harness.py
+++ lambda/src/Lambda/harness.py
@@ -1,6 +1,8 @@
 import argparse
+import json
 import os
 
+import boto3
 import awslambdaric.bootstrap
 
 from . import package_name, package_version
@@ -18,6 +20,8 @@ def parse_args():
 
     parser.add_argument("--log", default=env("LOG_LEVEL", "WARN"), help="set log level")
     parser.add_argument("--log-file", metavar="FILE", default=env("LOG_FILE"), help="redirect stdout and stderr to FILE")
+
+    parser.add_argument("--error-sns-topic-arn", metavar="TOPIC_ARN", default=env("ERROR_SNS_TOPIC_ARN"), help="publish errors to the SNS topic TOPIC_ARN")
 
     parser.add_argument("-C", "--directory", metavar="DIR", help="change to directory DIR before executing handler")
 
@@ -49,6 +53,21 @@ def setup_logger(level, ctx):
 
     return l, ch
 
+def region_of_arn(arn):
+    return arn.split(":")[3]
+
+def publish(topic_arn, subject, payload):
+    boto3.client("sns", region_name=region_of_arn(topic_arn)).publish(
+        TopicArn = topic_arn,
+        Subject = subject,
+        MessageStructure = "json",
+        Message = json.dumps({
+            "default": json.dumps(payload),
+            "SMS": subject,
+            "EMAIL": json.dumps(payload, indent=4),
+        }),
+    )
+
 def entrypoint(event, ctx):
     assert(args is not None)
     remove_default_logging()
@@ -64,6 +83,30 @@ def entrypoint(event, ctx):
         return main.main(event, ctx)
     except Exception as e:
         logger.error(e)
+
+        if args.error_sns_topic_arn:
+            region = os.environ["AWS_REGION"]
+
+            subject = f"{ctx.function_name} ({region}): {e}"
+
+            log_stream_url = f"https://{region}.console.aws.amazon.com/cloudwatch/home?region={region}#logEventViewer:group={ctx.log_group_name};stream={ctx.log_stream_name}"
+
+            payload = {
+                "execption": repr(e),
+                "event": event,
+                "invocation": {
+                    "function_arn": ctx.invoked_function_arn,
+                    "aws_request_id": ctx.aws_request_id,
+                    "log": {
+                        "group": ctx.log_group_name,
+                        "stream": ctx.log_stream_name,
+                        "url": log_stream_url,
+                    }
+                }
+            }
+
+            publish(topic_arn=args.error_sns_topic_arn, subject=subject, payload=payload)
+
         raise
     finally:
         handler.flush()
