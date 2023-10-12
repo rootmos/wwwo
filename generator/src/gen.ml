36fbf658ae34ae27d17770412c980fbb82d74b2b
Gustav Behm <me@rootmos.io>
2023-10-12 13:11:20 +0200

    Propagate build info into the generator

--- generator/src/gen.ml
+++ generator/src/gen.ml
@@ -89,9 +89,8 @@ let page ?(only_subtitle=false) ?(chartjs=false) ?(additional_css=[]) subtitle b
     div ~cls:(Some "content") @@ b;
     div ~cls:(Some "footer") @@ seq [
       let t = Unix.time () |> Unix.gmtime in
-      let c = Sys.getenv_opt "GIT_REV" |> Option.map (fun r ->
-        a (sprintf "https://github.com/rootmos/wwwo/commit/%s" r) @@
-        span @@ text (String.sub r 0 7)
+      let c = Sys.getenv_opt "BUILD_GIT_REF_ID" |> Option.map (fun r ->
+        a "version.html" @@ span @@ text (String.sub r 0 7)
       )
       in span ~cls:"copyleft" @@ seq @@ Option.to_list c @ [
         text @@ sprintf " %d &copy;" (1900 + t.tm_year)
