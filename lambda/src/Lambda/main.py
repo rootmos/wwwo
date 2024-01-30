9550e05123c1a078ebdb22f533c1d77b9bf634f8
Gustav Behm <me@rootmos.io>
2024-01-30 18:29:11 +0100

    Change the generate script to respect the BASE_URL variable

--- lambda/src/Lambda/main.py
+++ lambda/src/Lambda/main.py
@@ -19,16 +19,17 @@ def main(event, context):
     exe = os.path.abspath("generate")
     args = [ "generate", "-w", workdir.name, "-J16" ]
 
-    if env("TARGET"):
-        args += [ "-u", env("TARGET") ]
-
-        if env("BASE_URL"):
-            args += [ "-p", env("BASE_URL") ]
-
     env_ext = { **os.environ,
         "AWS_REQUEST_ID": context.aws_request_id,
         "AWS_LAMBDA_FUNCTION_ARN": context.invoked_function_arn,
     }
+
+    if env("TARGET"):
+        args += [ "-u", env("TARGET") ]
+
+    if env("BASE_URL"):
+        env_ext["BASE_URL"] = env("BASE_URL")
+        args += [ "-p" ]
 
     logger.debug(f"args: {args}")
     logger.debug(f"env: {env_ext}")
