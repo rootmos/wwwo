3ea3b5bc78c31dd75419b1afa7b2533c7d1cf918
Gustav Behm <me@rootmos.io>
2024-10-23 18:32:23 +0200

    Add some work towards fetching commits from GitHub

--- tasks/src/tasks/git_activity.py
+++ tasks/src/tasks/git_activity.py
@@ -1,3 +1,4 @@
+import collections
 import argparse
 import datetime
 import json
@@ -33,6 +34,50 @@ def from_github(u, N):
                     })
     return cs
 
+def github_token_from_env():
+    token = os.environ.get("GITHUB_TOKEN")
+    if token is None:
+        arn = os.environ["GITHUB_TOKEN_ARN"]
+        token = fetch_secret(arn)
+    return token
+
+def walk_github_repo(author_name, after):
+    visited = set()
+    found = set()
+    unvisited = collections.deque()
+
+    for branch in repo.get_branches():
+        unvisited.append(branch.commit)
+
+    while unvisited:
+        c = unvisited.pop()
+
+        if c in visited:
+            continue
+        visited.add(c)
+
+        if after and c.commit.author.date < after:
+            continue
+
+        if c.commit.author.name == author_name:
+            found.add(c)
+
+        for p in c.parents:
+            if p in visited:
+                continue
+            unvisited.append(p)
+
+    return found
+
+def fetch_from_github(author_name, username, after):
+    api = github.Github(login_or_token=token_from_env())
+
+    commits = set()
+    for repo in api.get_user(username).get_repos():
+        commits |= walk_github_repo(repo)
+
+    return commits
+
 def render_sourcehut_commit(c):
     return {
         "hash": c.id,
@@ -42,6 +87,7 @@ def render_sourcehut_commit(c):
         "repo": {
             "name": c.repo.name,
             "url": c.repo.url,
+            "public": c.repo.visibility == "PUBLIC",
         }
     }
 
@@ -72,8 +118,10 @@ def parse_args():
 
     parser.add_argument("--days", metavar='N', type=int, default=7)
 
+    parser.add_argument("--author-name", required=True)
+
     parser.add_argument("--github-username")
-    parser.add_argument("--sourcehut-author-name")
+    parser.add_argument("--sourcehut", action="store_true")
 
     return parser.parse_args()
 
@@ -87,12 +135,11 @@ def main():
     commits = []
 
     if args.github_username:
-        g = Github(fetch_secret(os.environ["GITHUB_TOKEN_ARN"]))
-        user = g.get_user(args.github_username)
-        commits += from_github(user, args.commits)
+        cs = fetch_from_github(author_name=args.author_name, username=args.github_username, after=after)
+        commits += [ render_github_commit(c) for c in cs ]
 
-    if args.sourcehut_author_name:
-        cs = fetch_from_sourcehut(author_name=args.sourcehut_author_name, after=after)
+    if args.sourcehut:
+        cs = fetch_from_sourcehut(author_name=args.author_name, after=after)
         commits += [ render_sourcehut_commit(c) for c in cs ]
 
     with output(args.output) as f:
