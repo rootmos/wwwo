0acbe28988f1824c89da5cbc35f7a8a74c16367b
Gustav Behm <me@rootmos.io>
2025-10-09 18:17:11 +0200

    Sketch the split intros

--- generator/src/gen.ml
+++ generator/src/gen.ml
@@ -214,107 +214,134 @@ let md_snippet s =
     | _ -> None in
   text @@ Omd.to_html md
 
-let acronym = "Rolling Oblong Ortofon Troubadouring Mystique Over Salaciousness"
+let slogan = div ~cls:(Some "slogan") @@ seq [
+  text "Some ";
+  a "#math" @@ text "math";
+  text ", ";
+  a "#music" @@ text "music";
+  text ", mostly ";
+  a "#programming" @@ text "programming";
+  text " and everything in between";
+]
+
+let rootmos_definition =
+  div ~cls:(Some "explanation") @@ seq [
+    text "rootmos := conflation of ";
+    a "https://en.wikipedia.org/wiki/Superuser" @@ text "root";
+    text " and ";
+    a "https://sv.wikipedia.org/wiki/Rotmos" @@ text "rotmos";
+  ]
+
+let sloth_explanation =
+  div ~cls:(Some "explanation") @@ seq [
+    text "and what's with the";
+    text " ";
+    a "https://knowyourmeme.com/memes/astronaut-sloth" @@ text "sloth";
+    text "?";
+  ]
+
+let trust obj trust verify = div ~cls:(Some "text") @@ seq [
+  text obj; text " ";
+  a ~alt:(Some "Reflections on trusting trust, Ken Thompson") "https://dl.acm.org/doi/10.1145/358198.358210" @@ text trust;
+  text ",&nbspbut ";
+  a "https://en.wikipedia.org/wiki/Trust,_but_verify" @@ text verify;
+]
+
+let description = function
+  0 -> [ slogan ]
+| 1 -> [
+  slogan;
+  div ~cls:(Some "separator") @@ noop;
+
+  rootmos_definition;
+  trust "I" "trust" "verify";
+  div ~cls:(Some "separator") @@ noop;
+
+  sloth_explanation;
+]
+| 2 -> [
+  slogan;
+
+  div ~cls:(Some "separator") @@ noop;
+
+  rootmos_definition;
+  div ~cls:(Some "text") @@ text "a problem-solving automaton";
+  trust "that" "trusts" "verifies";
+
+  div ~cls:(Some "separator") @@ noop;
+
+  div ~cls:(Some "text") @@ seq [
+    text "I like ";
+    a "https://github.com/rootmos/silly-k" @@ text "silly";
+    text " things and ";
+    a "https://en.wikipedia.org/w/index.php?title=Surreal_Numbers_(book)" @@ text "surreal";
+    text " non-sense";
+  ];
+  div ~cls:(Some "text") @@ text "but I'm dead serious about code and ruthlessly (self-)critical";
+
+  div ~cls:(Some "separator") @@ noop;
+
+  div ~cls:(Some "text") @@ seq [
+    text "oh, and yes I'm ";
+    a "https://unixgreybeard.com/" @@ text "that guy";
+    text ":";
+  ];
+  div ~cls:(Some "text") @@ seq [
+    a "https://archlinux.org/" @@ text "Arch Linux";
+    text ", ";
+    a "https://github.com/rootmos/desktop" @@ text ".";
+    text "&thinsp;";
+    a "https://xmonad.org/" @@ text "XMonad";
+    text ", ";
+    a "https://en.wikipedia.org/wiki/Kinesis_(keyboard)#Contoured_/_Advantage" @@ text "Kinesis";
+    text ", ";
+    a "https://github.com/rootmos/dvorak" @@ text "custom";
+    text " ";
+    a "https://en.wikipedia.org/wiki/Dvorak_keyboard_layout" @@ text "Dvorak";
+    text " and ";
+    a "https://git.sr.ht/~rootmos/dot-nvim" @@ text ".";
+    text "&thinsp;";
+    a "https://neovim.io/" @@ text "nvim";
+  ];
+
+  div ~cls:(Some "separator") @@ noop;
+
+  sloth_explanation;
+]
+| _ -> failwith "undefined variant"
 
 let avatar variant = seq [
-  img ~cls:(Some "avatar") ~alt:(Some acronym)
-      (Path.image "rootmos.jpg");
-  div ~cls:(Some "description") @@ seq @@ List.rev [
-    div ~cls:(Some "slogan") @@ seq [
-      text "Some ";
-      a "#math" @@ text "math";
-      text ", ";
-      a "#music" @@ text "music";
-      text ", mostly ";
-      a "#programming" @@ text "programming";
-      text " and everything in between";
-    ];
+  begin match variant with
+    0 -> img ~cls:(Some "portrait") ~alt:(Some "Gustav Behm") (Path.image "portrait.jpg")
+  | _ ->
+      let acronym = "Rolling Oblong Ortofon Troubadouring Mystique Over Salaciousness" in
+      img ~cls:(Some "avatar") ~alt:(Some acronym) (Path.image "rootmos.jpg")
+  end;
 
-    div ~cls:(Some "separator") @@ noop;
-
-    div ~cls:(Some "explanation") @@ seq [
-      text "rootmos := conflation of ";
-      a "https://en.wikipedia.org/wiki/Superuser" @@ text "root";
-      text " and ";
-      a "https://sv.wikipedia.org/wiki/Rotmos" @@ text "rotmos";
-    ];
-    div ~cls:(Some "text") @@ text "a problem-solving automaton";
-    div ~cls:(Some "text") @@ seq [
-      (*text "I ";*)
-      text "that ";
-      a ~alt:(Some "Reflections on trusting trust, Ken Thompson") "https://dl.acm.org/doi/10.1145/358198.358210" @@ text "trusts";
-      text ",&nbspbut ";
-      a "https://en.wikipedia.org/wiki/Trust,_but_verify" @@ text "verifies";
-    ];
-
-    div ~cls:(Some "separator") @@ noop;
-
-    div ~cls:(Some "text") @@ seq [
-      text "I like ";
-      a "https://github.com/rootmos/silly-k" @@ text "silly";
-      text " things and ";
-      a "https://en.wikipedia.org/w/index.php?title=Surreal_Numbers_(book)" @@ text "surreal";
-      text " non-sense";
-    ];
-    div ~cls:(Some "text") @@ text "but I'm dead serious about code and ruthlessly (self-)critical";
-
-    div ~cls:(Some "separator") @@ noop;
-
-    div ~cls:(Some "text") @@ seq [
-      text "oh, and yes I'm ";
-      a "https://unixgreybeard.com/" @@ text "that guy";
-      text ":";
-    ];
-    div ~cls:(Some "text") @@ seq [
-      a "https://archlinux.org/" @@ text "Arch Linux";
-      text ", ";
-      a "https://github.com/rootmos/desktop" @@ text ".";
-      text "&thinsp;";
-      a "https://xmonad.org/" @@ text "XMonad";
-      text ", ";
-      a "https://en.wikipedia.org/wiki/Kinesis_(keyboard)#Contoured_/_Advantage" @@ text "Kinesis";
-      text ", ";
-      a "https://github.com/rootmos/dvorak" @@ text "custom";
-      text " ";
-      a "https://en.wikipedia.org/wiki/Dvorak_keyboard_layout" @@ text "Dvorak";
-      text " and ";
-      a "https://git.sr.ht/~rootmos/dot-nvim" @@ text ".";
-      text "&thinsp;";
-      a "https://neovim.io/" @@ text "nvim";
-    ];
-
-    div ~cls:(Some "separator") @@ noop;
-
-    div ~cls:(Some "explanatio") @@ seq [
-      text "and what's with the";
-      text " ";
-      a "https://knowyourmeme.com/memes/astronaut-sloth" @@ text "sloth";
-      text "?";
-    ];
-  ];
+  div ~cls:(Some "description") @@ seq @@ List.rev @@ description variant;
 ]
 
 let index variant posts_snippet =
   pagemaker config Default ~back:None ~additional_css:[ "twitch.css" ] @@ seq [
-  div ~cls:(Some "intro") @@ seq [
-    div ~cls:(Some "row") @@ avatar variant;
-    div ~cls:(Some "row") @@ seq [
-      social;
+    div ~cls:(Some "intro") @@ seq [
+      div ~cls:(Some "row") @@ avatar variant;
+      div ~cls:(Some "row") @@ seq [
+        social;
+      ];
     ];
-  ];
 
-  div ~id:(Some "programming") ~cls:(Some "content") @@ projects_snippet;
-  div ~cls:(Some "content") @@ Activity.snippet ();
-  div ~cls:(Some "content") @@ posts_snippet;
+    div ~id:(Some "programming") ~cls:(Some "content") @@ projects_snippet;
+    div ~cls:(Some "content") @@ Activity.snippet ();
+    div ~cls:(Some "content") @@ posts_snippet;
 
-  div ~id:(Some "music") ~cls:(Some "content") @@ sounds_snippet;
-  div ~cls:(Some "content twitch") @@ twitch_snippet;
+    div ~id:(Some "music") ~cls:(Some "content") @@ sounds_snippet;
+    div ~cls:(Some "content twitch") @@ twitch_snippet;
 
-  div ~cls:(Some "content") @@ services_snippet;
+    div ~cls:(Some "content") @@ services_snippet;
 
-  div ~id:(Some "math") ~cls:(Some "content") @@ md_snippet (Path.snippet "academic.md");
-  div ~cls:(Some "content") @@ resume_snippet;
-]
+    div ~id:(Some "math") ~cls:(Some "content") @@ md_snippet (Path.snippet "academic.md");
+    div ~cls:(Some "content") @@ resume_snippet;
+  ]
 
 let bor19 = seq [
   p ~cls:(Some "c") @@ img ~cls:(Some "cover") ~alt:(Some "cover")
